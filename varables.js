75b0d7a7243a969aeea352f6dce8e3bab3034521
Roshanell Francisco <roshanellf@gmail.com>
2022-12-07 18:42:23 -0600

    intial commit

---
+++ varables.js
@@ -0,0 +1,132 @@
+// Week 1 - Variables
+
+// In the following exercises, you will need to place your code or answer underneath each
+// exercise prompt.
+
+// First try answering these without using references or looking up any information.
+// Then, check your answer by using references and/or running your code.
+// You can run your JS code using the Chrome or Firefox Developer tools, or by using Node.js.
+// Feel free to update your answers if you got them wrong at first -- this exercise is for your own learning.
+// But make sure you understand why the correct answer is right.
+
+// Data Types and Variables
+
+// Exercise 1. Simply declare a variable named 'emptyVariable'
+
+let emptyVariable =
+
+// Exercise 2. Declare a variable called 'myName'
+let  myName
+
+// Exercise 3. Assign your name as the value for 'myName'
+const myName = 'Roshanell'
+// Exercise 4. Declare a variable called dreamDestination and give it the value
+// of what your dream vacation destination is.
+const dreamDestination = 'Thiland'
+// Exercise 5. Make the following variables and assignments
+// - a variable called num1 with a value greater than 1 and less than 10
+// - a variable called num2 with a value of greater than or equal to 1 and less
+// than or equal to 10
+let num1 = 6
+let num2 = 9
+// Exercise 6. Write a comment below explaining the difference between the rules
+// for num1 and num2 from Exercise 5. What numbers would be valid values for
+// num2 but not num1?
+
+// num1 must be assigned a number that is between the numbers 1 and 10 not including 10 or the number one. This means the number can be anything between 2 and 9
+// num2 must be assigned a number that starts with the number one and ends with the number 10. This means the number can be anything from 1 to 10.
+
+// Exercise 7. Now we will try some addition.
+// a. Assign the the values of 4 and 6 to num1 and num2, respectively.
+// b. Make a new variable called 'theSum', and use 'num1' and
+// 'num2' to assign its value using the "+" operator.
+let num1 = 4;
+let num2 = 6;
+
+const theSum = num1 + num2
+
+// Exercise 8. Now we will try some multiplication.
+// Make a new variable called 'theProduct', multiply num1 and num2 and assign
+// the resulting value to 'theProduct'.
+
+const theProduct = num1 * num2;
+
+// Exercise 9.
+// Make a new variable called 'statement' and using 'myName' and
+// 'dreamDestination' and the concatenation method of your choice, make the
+// value of statement to be:
+// "Hi, my name is <your name>, and I can't wait to visit <your destination>!"
+
+// *NOTE* For the following exercises, put your answers in the line below
+// the description of each exercise. For example, below a prompt, you may see:
+// let myAnswerForExercise10= "";
+// If your answer is "foo", then fill in the line to be:
+// let myAnswerForExercise10 = "foo";
+
+let statement = "Hi my name is" + myName + ", and I can't wait to visit " + dreamDestination + "!"
+
+// Exercise 10. What is the data type of the value stored in 'dreamDestination',
+// place your answer in the quotes below:
+
+const myAnswerForExercise10 = "string";
+
+// Exercise 11. What is the data type of the value stored in 'product',
+// place your answer in the quotes below:
+
+const myAnswerForExercise11 = "number";
+
+// Exercise 12. What do you think the value of emptyVariable is?
+// Place your answer below:
+
+const myAnswerForExercise12 = "null";
+
+// Exercise 13. If
+const A = "R";
+const B = 1;
+const C = 4;
+const D = "D";
+
+// What is the value of B + C
+const myAnswerForExercise13 = "5";
+
+// Exercise 14 What is the value of
+// A + "n" + D
+const myAnswerForExercise14 = "RnD";
+
+// Exercise 15 What is the value of
+// A + B * 2 + D + C / 2:
+const myAnswerForExercise15 = "R2D2";
+
+// Exercise 16 What is the value of A * B
+const myAnswerForExercise16 = "NaN";
+// number times alphabet results in NaN
+
+// Exercise 17 What is the value of true || false
+const myAnswerForExercise17 = true;
+
+// why does it always say true?
+
+// Exercise 18 What is the value of 1 === "1"
+const myAnswerForExercise18 = "false";
+
+// Exercise 19 What is the value of 1 == "1"
+const myAnswerForExercise19 = "true";
+
+// Exercise 20 What is the value of 1 < 1
+const myAnswerForExercise20 = "false";
+
+// Exercise 21 What is the value of 1 < 2 < 3
+const myAnswerForExercise21 = "true";
+
+// Exercise 22 What is the value of 2 < 1 < 3
+const myAnswerForExercise22 = "false";
+
+// Congrats, you made it to the end!
+// Did you find this easy or hard? If you used references, which ones helped you?
+// Please answer in a comment below.
+//
+/* This was fairly easy, problems 15-17 were a little different for me. I never tried multiplying numbers, so that was interesting. I have tried adding numbers, so I know that concatenation would happen in those instances. I also was a bit confused with the true or false question. I wasn't sure if that would be truthy or falsy. */
+
+
+// Email your file to the course staff,
+// or commit your file to GitHub and email us a link.// Week 1 - Variables
